3050509f907f8369774c734d670ea8203595c527
Faizan Afzal <148323355+Faizan-Afzal2@users.noreply.github.com>
2025-04-26 14:12:24 +0500

    basic setup and database connection

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip unallowed props
+      forbidNonWhitelisted: true, // Throw error on unknown props
+      transform: true, // Transform payloads to DTO instances
+    }),
+  );
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  await app.listen(3000);
+}
+bootstrap();
